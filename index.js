483d8f0aabe99a27d1923b3f45cff478b59d8ae6
evertonoliveira <contato.evertonoliveira@hotmail.com>
2019-08-20 19:24:23 -0300

    feat: start text robot

---
+++ index.js
@@ -0,0 +1,31 @@
+const readline = require('readline-sync')
+const robots = {
+    text: require('./robots/text.js')
+}
+
+async function start(){
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
+    await robots.text(content)
+
+    //Função para retornar na tela um input para interação com o usuário
+    function askAndReturnSearchTerm(){
+        return readline.question('Digite termo para a pesquisa no Wikipedia: ')
+    }
+
+    //Definindo prefixos para a busca
+    function askAndReturnPrefix(){
+        const prefixes = ['Who is','What is','The history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes)
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+        return selectedPrefixText
+    }
+
+    console.log(content)
+}
+
+start()
